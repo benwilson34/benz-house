5e6796ae9960aafe4d3ae4c45efcec9fabcc3778
Ben Wilson <ben.wilson1337@gmail.com>
2024-12-13 18:52:49 -0500

    fix resume download name

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -64,7 +64,7 @@ export default function Index() {
         ,{" "}
         <a
           href="../WilsonBenjamin_Resume_2024-10-21_WebAppFocus.pdf"
-          download="WilsonBenjamin_Resume_2024-10-28.pdf"
+          download="WilsonBenjamin_Resume_2024-10-21.pdf"
         >
           my résumé
         </a>
