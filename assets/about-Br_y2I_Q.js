ebb1299fc1997465ee92a5f8a6477bb553f931b4
GitHub Action <action@github.com>
2024-12-19 21:52:36 +0000

    publish latest site

---
+++ assets/about-Br_y2I_Q.js
@@ -0,0 +1,9 @@
+import{j as t}from"./jsx-runtime-BMrMXMSG.js";import{f as r,A as a}from"./ArticleHeader-BHYaRTsD.js";const s={title:"about this site",datePublished:"2024-12-16T00:00:00-05:00"},d=()=>[{title:r(s.title)}];function n(i){const e={a:"a",h1:"h1",hr:"hr",p:"p",...i.components};return t.jsxs(t.Fragment,{children:[t.jsx(e.h1,{id:"",children:s.title}),`
+`,t.jsx(a,{datePublished:s.datePublished,dateUpdated:s.dateUpdated}),`
+`,t.jsx(e.hr,{}),`
+`,t.jsxs(e.p,{children:["This is a static site powered by ",t.jsx(e.a,{href:"https://remix.run/",children:"Remix"}),", ",t.jsx(e.a,{href:"https://tailwindcss.com/",children:"Tailwind"}),", and ",t.jsx(e.a,{href:"https://mdxjs.com/",children:"MDX"}),". The ",t.jsx(e.a,{href:"https://github.com/benwilson34/benz-house",children:"source code"})," is publically available; feel free to use the project as a starting point for your own site."]}),`
+`,t.jsxs(e.p,{children:["If you'd like to learn more about how this site was made, I've written an article about it: ",t.jsx(e.a,{href:"/posts/how-to-make-a-static-blog-with-remix-and-mdx",children:"how to make a static blog with Remix and MDX"})]}),`
+`,t.jsx(e.hr,{}),`
+`,t.jsx(e.p,{children:"Title font is Trickster by Jean-Baptiste Morizot. Distributed by velvetyne.fr."}),`
+`,t.jsx(e.p,{children:"Copy font is Clash Display by Indian Type Foundry. Distributed by Fontshare."}),`
+`,t.jsxs(e.p,{children:["ASCII art font is IBM PC BIOS (CP437). Distributed by VileR via ",t.jsx(e.a,{href:"https://int10h.org/oldschool-pc-fonts/",children:"The Oldschool PC Font Resource"}),"."]})]})}function l(i={}){const{wrapper:e}=i.components||{};return e?t.jsx(e,{...i,children:t.jsx(n,{...i})}):n(i)}export{l as default,d as meta};
---
+++ assets/posts.test-post-DWhaVJKw.js
@@ -0,0 +1,36 @@
+import{j as e}from"./jsx-runtime-BMrMXMSG.js";const a={meta:{title:"My First Post",name:"description",content:"Isn't this just awesome?"}};function n(i){const s={a:"a",blockquote:"blockquote",code:"code",h1:"h1",h2:"h2",h3:"h3",hr:"hr",li:"li",ol:"ol",p:"p",pre:"pre",span:"span",ul:"ul",...i.components};return e.jsxs(e.Fragment,{children:[e.jsx(s.h1,{id:"example-markdown-post",className:"heading-link",children:e.jsx(s.a,{href:"#example-markdown-post",children:"Example Markdown Post"})}),`
+`,e.jsxs(s.p,{children:["You can reference your frontmatter data through ",e.jsx(s.code,{children:"frontmatter"}),". The title of this post is ",a.meta.title,"!"]}),`
+`,e.jsxs(s.blockquote,{children:[`
+`,e.jsx(s.p,{children:"Lorem ipsum odor amet, consectetuer adipiscing elit. Ante tempus mus feugiat dignissim platea magnis tellus. Est aliquam curabitur dictum curae suscipit. Phasellus pellentesque aliquam potenti aliquet porttitor. Primis sollicitudin fermentum ut tempor efficitur himenaeos leo. Pretium lobortis lacinia porttitor arcu id imperdiet bibendum. Massa adipiscing semper quam potenti sit sed enim phasellus."}),`
+`]}),`
+`,e.jsx(s.p,{children:"Dui adipiscing lectus imperdiet orci lobortis mus. Per gravida quam fermentum tortor, imperdiet class enim. Blandit primis in sapien potenti sem lectus etiam fames. Posuere luctus fringilla curabitur donec diam quisque cursus himenaeos. Lacinia felis bibendum hendrerit commodo facilisis? Proin conubia non eget aliquet suscipit. Metus hac leo condimentum taciti felis mi gravida commodo."}),`
+`,e.jsx(s.hr,{}),`
+`,e.jsx(s.h2,{id:"heading-2",className:"heading-link",children:e.jsx(s.a,{href:"#heading-2",children:"Heading 2"})}),`
+`,e.jsxs(s.p,{children:["Here's ",e.jsx(s.a,{href:"https://google.com",children:"something cool"})," to look at."]}),`
+`,e.jsxs(s.p,{children:["Here's ",e.jsx(s.code,{children:"blah"})," which is a thing."]}),`
+`,e.jsx(s.pre,{children:e.jsxs(s.code,{className:"hljs language-ts",children:[e.jsx(s.span,{className:"hljs-keyword",children:"type"})," ",e.jsx(s.span,{className:"hljs-title class_",children:"Foo"}),` = {
+  `,e.jsx(s.span,{className:"hljs-attr",children:"a"}),": ",e.jsx(s.span,{className:"hljs-built_in",children:"string"}),`;
+  `,e.jsx(s.span,{className:"hljs-attr",children:"b"}),": ",e.jsx(s.span,{className:"hljs-built_in",children:"number"}),`[];
+  `,e.jsx(s.span,{className:"hljs-attr",children:"c"}),"?: ",e.jsx(s.span,{className:"hljs-title class_",children:"Date"}),`;
+};
+
+`,e.jsx(s.span,{className:"hljs-keyword",children:"const"})," ",e.jsx(s.span,{className:"hljs-attr",children:"myFoo"}),": ",e.jsx(s.span,{className:"hljs-title class_",children:"Foo"}),` = {
+  `,e.jsx(s.span,{className:"hljs-attr",children:"a"}),": ",e.jsx(s.span,{className:"hljs-string",children:'"bar"'}),`,
+  `,e.jsx(s.span,{className:"hljs-attr",children:"b"}),": ",e.jsx(s.span,{className:"hljs-string",children:'"baz"'}),`,
+};
+`]})}),`
+`,e.jsx(s.h3,{id:"heading-3",className:"heading-link",children:e.jsx(s.a,{href:"#heading-3",children:"Heading 3"})}),`
+`,e.jsx(s.p,{children:"Dui adipiscing lectus imperdiet orci lobortis mus. Per gravida quam fermentum tortor, imperdiet class enim. Blandit primis in sapien potenti sem lectus etiam fames. Posuere luctus fringilla curabitur donec diam quisque cursus himenaeos. Lacinia felis bibendum hendrerit commodo facilisis? Proin conubia non eget aliquet suscipit. Metus hac leo condimentum taciti felis mi gravida commodo."}),`
+`,e.jsxs(s.ol,{children:[`
+`,e.jsx(s.li,{children:"Placerat"}),`
+`,e.jsx(s.li,{children:"penatibus"}),`
+`,e.jsx(s.li,{children:"lacus fusce;"}),`
+`,e.jsx(s.li,{children:"amet taciti eu."}),`
+`]}),`
+`,e.jsxs(s.ul,{children:[`
+`,e.jsx(s.li,{children:"blah1"}),`
+`,e.jsx(s.li,{children:"blah2"}),`
+`,e.jsx(s.li,{children:"blah3"}),`
+`,e.jsx(s.li,{children:"blah4"}),`
+`,e.jsx(s.li,{children:"blah5"}),`
+`]})]})}function t(i={}){const{wrapper:s}=i.components||{};return s?e.jsx(s,{...i,children:e.jsx(n,{...i})}):n(i)}export{t as default};
