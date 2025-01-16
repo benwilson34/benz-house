a6d05e4162b327acefc5d52eae1af414fb38beb8
GitHub Action <action@github.com>
2025-01-16 21:32:40 +0000

    publish latest site

---
+++ assets/posts.favorite-albums-of-2021-DtePwQk-.js
@@ -0,0 +1,24 @@
+import{j as n}from"./jsx-runtime-BMrMXMSG.js";import{f as t,A as l}from"./ArticleHeader-CALjWnEo.js";const s={title:"my favorite albums of 2021",datePublished:"2021-12-31T00:00:00-05:00"},o=()=>[{title:t(s.title)}];function r(i){const e={h1:"h1",hr:"hr",li:"li",ol:"ol",p:"p",strong:"strong",ul:"ul",...i.components};return n.jsxs(n.Fragment,{children:[n.jsx(e.h1,{id:"",children:s.title}),`
+`,n.jsx(l,{datePublished:s.datePublished,dateUpdated:s.dateUpdated}),`
+`,n.jsx(e.hr,{}),`
+`,n.jsx(e.p,{children:"With two hours left in 2021 I've decided my top ten albums from this year! Which of your faves did I miss? 😳"}),`
+`,n.jsxs(e.ol,{children:[`
+`,n.jsxs(e.li,{children:[n.jsx(e.strong,{children:"Hiatus Kaiyote - Mood Valiant"}),` ⭐ "All The Words We Don't Say"`]}),`
+`,n.jsxs(e.li,{children:[n.jsx(e.strong,{children:"The Armed - Ultrapop"}),' ⭐ "AN ITERATION"']}),`
+`,n.jsxs(e.li,{children:[n.jsx(e.strong,{children:"Rochelle Jordan - PLAY WITH THE CHANGES"}),' ⭐ "COUNT IT"']}),`
+`,n.jsxs(e.li,{children:[n.jsx(e.strong,{children:"Machinedrum - Psyconia EP"}),' ⭐ "Only One (ft. Angelica Bess)"']}),`
+`,n.jsxs(e.li,{children:[n.jsx(e.strong,{children:"JPEGMAFIA - LP!"}),' ⭐ "TIRED, NERVOUS, & BROKE!"']}),`
+`,n.jsxs(e.li,{children:[n.jsx(e.strong,{children:"BODY MEAT - Year of the Orc EP"}),' ⭐ "Ghost (ft. Laraaji)"']}),`
+`,n.jsxs(e.li,{children:[n.jsx(e.strong,{children:"Sun People - Transitions EP"}),' ⭐ "Transitions"']}),`
+`,n.jsxs(e.li,{children:[n.jsx(e.strong,{children:"BROCKHAMPTON - ROADRUNNER: NEW LIGHT, NEW Machine"}),' ⭐ "BUZZCUT (ft. Danny Brown)"']}),`
+`,n.jsxs(e.li,{children:[n.jsx(e.strong,{children:"Iglooghost - Lei Line Eon"}),' ⭐ "Pure Grey Circle"']}),`
+`,n.jsxs(e.li,{children:[n.jsx(e.strong,{children:"Ross From Friends - Tread"}),' ⭐ "XXX Olympiad"']}),`
+`]}),`
+`,n.jsx(e.p,{children:"Barely didn't make it:"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:"Frontierer - Oxidized"}),`
+`,n.jsx(e.li,{children:"Deerhoof - Actually, You Can"}),`
+`,n.jsx(e.li,{children:"NNAMDI - Are You Happy"}),`
+`,n.jsx(e.li,{children:"Alfa Mist - Bring Backs"}),`
+`]}),`
+`,n.jsx(e.p,{children:"Can't wait for what comes out in 2022!!"})]})}function d(i={}){const{wrapper:e}=i.components||{};return e?n.jsx(e,{...i,children:n.jsx(r,{...i})}):r(i)}const h=Object.freeze(Object.defineProperty({__proto__:null,default:d,frontmatter:s,meta:o},Symbol.toStringTag,{value:"Module"}));export{d as M,h as _,o as m};
---
+++ assets/posts.favorite-albums-of-2022-D9IEL_UE.js
@@ -0,0 +1,16 @@
+import{j as e}from"./jsx-runtime-BMrMXMSG.js";import{f as r,A as o}from"./ArticleHeader-CALjWnEo.js";const l={title:"my favorite albums of 2022",datePublished:"2023-01-04T00:00:00-05:00"},a=()=>[{title:r(l.title)}];function i(t){const n={h1:"h1",hr:"hr",li:"li",ol:"ol",p:"p",...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.h1,{id:"",children:l.title}),`
+`,e.jsx(o,{datePublished:l.datePublished,dateUpdated:l.dateUpdated}),`
+`,e.jsx(n.hr,{}),`
+`,e.jsx(n.p,{children:"My favorite albums of 2022 only a couple days late! So much good music last year 🥰 what was your fave?!"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"death's dynamic shroud - Darklife"}),`
+`,e.jsx(n.li,{children:"Quelle Chris - DEATHFAME"}),`
+`,e.jsx(n.li,{children:"Machine Girl - Neon White OST"}),`
+`,e.jsx(n.li,{children:"JID - The Forever Story"}),`
+`,e.jsx(n.li,{children:"Culprate - αριθμός τέσσερα (Number Four)"}),`
+`,e.jsx(n.li,{children:"Tennyson - Rot"}),`
+`,e.jsx(n.li,{children:"black midi - Hellfire"}),`
+`,e.jsx(n.li,{children:"Toro y Moi - MAHAL"}),`
+`,e.jsx(n.li,{children:"Viagra Boys - Cave World"}),`
+`,e.jsx(n.li,{children:"Hudson Mohawke - Cry Sugar"}),`
+`]})]})}function s(t={}){const{wrapper:n}=t.components||{};return n?e.jsx(n,{...t,children:e.jsx(i,{...t})}):i(t)}const h=Object.freeze(Object.defineProperty({__proto__:null,default:s,frontmatter:l,meta:a},Symbol.toStringTag,{value:"Module"}));export{s as M,h as _,a as m};
